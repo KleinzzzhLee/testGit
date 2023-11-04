aa18cb95055ad9dc384ee5efef36158d68aa45bf
zzzhlee <zzzhlee0930@163.com>
2023-11-04 17:02:53 +0800

    first branch

--- src/SayHello.java
+++ src/SayHello.java
@@ -3,5 +3,8 @@ public class SayHello {
         System.out.println("Say hello!!!");
         System.out.println("哈哈哈啊哈");
         System.out.println("我又来了！！");
+        if(true) {
+            System.out.println("我更改了！！！！");
+        }
     }
 }

f74012ea3c8e2f0d10decf45980ca3a4e6a4a487
zzzhlee <zzzhlee0930@163.com>
2023-11-04 16:53:30 +0800

    second commit

--- src/SayHello.java
+++ src/SayHello.java
@@ -2,5 +2,6 @@ public class SayHello {
     public static void main(String[] args) {
         System.out.println("Say hello!!!");
         System.out.println("哈哈哈啊哈");
+        System.out.println("我又来了！！");
     }
 }
